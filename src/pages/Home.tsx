49a152b122d64599759743bd6b7ac1855f7881e0
KiranNaikS12 <kiransnaik56@gmail.com>
2025-03-26 18:39:24 +0530

    feat(ui): Adjusted layout and styling to match the given design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,21 +10,22 @@ function Home() {
 
   const toggleModal = useCallback(() => {
     setIsModalOpen((prev) => !prev)
-  },[isModalOpen])
+  },[])
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
       <Toaster position="top-right" />
       <div className="container px-4 py-8 mx-auto">
-        <div>
+        <div className='flex justify-between'>
           <div className="flex items-center gap-3">
             <Clock className="w-8 h-8 text-blue-600" />
             <h1 className="text-3xl font-bold text-gray-900">Timer</h1>
           </div>
           <Button onClick={toggleModal}
+            title='Add Timer'
             label='Add Timer'
             className="flex items-center gap-2 px-4 py-2 text-white transition-colors bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg"
-            icon={<Plus w-5 h-5 />}
+            icon={<Plus className="w-5 h-5"  />}
           />
         </div>
 
